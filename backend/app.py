58993a6b78972b815c59547c111364f4cdbd4334
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-01-08 21:28:50 +0000

    backend logic added

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,60 @@
-from flask import Flask, request, jsonify, session
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, render_template
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
 
 # config your host & port for app
 HOST = "0.0.0.0"
 PORT = 5004
 
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.Text)
+# config db
+engine = create_engine('sqlite:///blog.db')
+Base = declarative_base()
+
+class Blog(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 
 @app.route('/post', methods=['POST'])
 def create_post():
-    # create a new post
-    new_post = Blog(title=request.form['title'], content=request.form['content'])
-    db.session.add(new_post)
-    db.session.commit()
-    return 'Success'
+    # Get the data for the new post from the request
+    title = request.form['title']
+    content = request.form['content']
 
+    # Create a new post object
+    post = Blog(title=title, content=content)
+
+    # Add the post to the database
+    session.add(post)
+    session.commit()
+
+    return 'New blog created successfully!'
+
+
+# backend api
 @app.route('/post/<int:post_id>')
 def get_post(post_id):
     # get a specific post
     post = Blog.query.get(post_id)
-    return post.title
+    res = {
+        'id': post.id,
+        'title': post.title,
+        'content': post.content
+    }
+    return jsonify(res)
 
-# backend api
+
 @app.route('/posts')
 def get_posts():
     # Fetch all posts from the database
