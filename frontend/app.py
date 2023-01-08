50cf65397c114941c082198fa6a0612372b86a0a
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-01-08 18:54:59 +0000

    create blog html page added

--- frontend/app.py
+++ frontend/app.py
@@ -12,6 +12,10 @@ PORT = 5003
 def home():
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    return render_template('create_blog.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host=HOST, port=PORT)
