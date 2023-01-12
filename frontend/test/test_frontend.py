8bb36902300fedb97c15b1fde9d7b99e52c8615b
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-01-12 13:40:22 +0000

    pytest set up for frontend but not complete

---
+++ frontend/test/test_frontend.py
@@ -0,0 +1,24 @@
+import pytest
+from flask import Flask
+from frontend.app import app
+
+@pytest.fixture
+def app():
+    app = app
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Welcome to my Blog' in response.data
+
+
+def test_create_blog(client):
+    response = client.post('/create', data={'title': 'Test Title', 'content': 'Test Content'})
+    assert response.status_code == 200
